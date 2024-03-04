43a85db64669cffcd7207d902a4895097c3e3a25
Mayank Yadav <ymayank521@gmail.com>
2024-03-05 03:13:12 +0530

    added JwtConstant and completed JwtTokenValidator

---
+++ efood-backend/src/main/java/com/restaurant/onlinefood/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.restaurant.onlinefood.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="sfgagfsdfsdbsdfagdadgadfbbngmhffnakngksnfkankajfna";
+    public static final String JWT_HEADER="Authorization";
+}
