567e19d9bc6209cbe39b4d64cee24d28fb3d17d9
Mayank Yadav <ymayank521@gmail.com>
2024-03-09 02:06:08 +0530

    Implemented Admin restaurant controller

---
+++ efood-backend/src/main/java/com/restaurant/onlinefood/controller/RestaurantController.java
@@ -0,0 +1,4 @@
+package com.restaurant.onlinefood.controller;
+
+public class RestaurantController {
+}
---
+++ efood-backend/src/main/java/com/restaurant/onlinefood/response/MessageResponse.java
@@ -0,0 +1,8 @@
+package com.restaurant.onlinefood.response;
+
+import lombok.Data;
+
+@Data
+public class MessageResponse {
+    private String message;
+}
